9edb1770500d6c4bf4fd9764990e2b0310b5f302
paolonit <46544340+paolonit@users.noreply.github.com>
2023-04-02 22:26:13 -0400

    Create main.tf

---
+++ program/main.tf
@@ -0,0 +1,25 @@
+data "azuread_client_config" "current" {}
+
+variable "center_name" {
+  type        = string
+  description = "The name of the program being onboarded."
+  nullable = false
+}
+
+variable "program_name" {
+  type        = string
+  description = "The name of the program being onboarded."
+  nullable = false
+}
+
+resource "azuread_group" "program_aad_group" {
+  display_name     = var.program_name
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.program_name
+  storage_account_name  = "swordfish${var.center_name}"
+  container_access_type = "private"
+}
