111316f8f811b9a1d753960d9be52b8097687c55
paolonit <46544340+paolonit@users.noreply.github.com>
2023-04-02 23:59:10 -0400

    Update main.tf

--- program/main.tf
+++ program/main.tf
@@ -45,12 +45,11 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "program_storage_container"
   provider              = azurerm
   name                  = var.program_name
   storage_account_id  = "/subscriptions/df0394a0-26f7-4700-9fbb-5d0df992d9de/resourceGroups/rg-dev/providers/Microsoft.Storage/storageAccounts/swordfish${var.center_name}"
-  container_access_type = "private"
-}
-
-resource "azurerm_role_assignment" "program_storage_container_role_assignment" {
-  provider              = azurerm
-  scope                = resource.azurerm_storage_container.program_storage_container.resource_manager_id
-  role_definition_name = "Contributor"
-  principal_id         = resource.azuread_group.program_aad_group.object_id
+    
+  ace {
+    type = "group"
+    scope = "access"
+    id = resource.azuread_group.program_aad_group.object_id
+    permissions = "rwx"
+  }
 }
