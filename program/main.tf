3c80d9095b071d8131d04280309e496f463f92d9
paolonit <46544340+paolonit@users.noreply.github.com>
2023-04-02 22:51:34 -0400

    Update main.tf

--- program/main.tf
+++ program/main.tf
@@ -1,28 +1,49 @@
-provider "azurerm" {
-   features {}
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.36.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.50.0"
+    }
+  }
 }
 
-data "azuread_client_config" "current" {}
+provider "azuread" {
+  features {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azuread_client_config" "current" {
+  provider = azuread
+}
 
 variable "center_name" {
   type        = string
   description = "The name of the program being onboarded."
-  nullable = false
+  nullable    = false
 }
 
 variable "program_name" {
   type        = string
   description = "The name of the program being onboarded."
-  nullable = false
+  nullable    = false
 }
 
 resource "azuread_group" "program_aad_group" {
+  provider         = azuread
   display_name     = var.program_name
   owners           = [data.azuread_client_config.current.object_id]
   security_enabled = true
 }
 
 resource "azurerm_storage_container" "program_storage_container" {
+  provider              = azurerm
   name                  = var.program_name
   storage_account_name  = "swordfish${var.center_name}"
   container_access_type = "private"
